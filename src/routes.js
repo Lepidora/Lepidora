5c0cee9e64810625b7c9b5a6c87b23b547de5a10
Lepidora <miamukherjee2+lepidora@gmail.com>
2018-07-05 16:47:35 +0100

    Added project pages
    Now serves project pages. Updated some other routes.

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,26 @@
  * Routing for pages
  */
 
+var path = require('path');
+
 var index = require('./routes/index')
   , pages = require('./routes/pages')
-  , toys = require('./routes/toys');
+  , toys = require('./routes/toys')
+  , projects = require('./controllers/projects.js');
 
 module.exports = function(app) {
 	
 	app.get('/', index.render);
-	app.get('/toys/emojitext', toys.emojitext)
+	app.get('/emojitext', toys.emojitext);
+	app.get('/project/:project', renderProject());
+	app.get('/reloadprojects', projects.reloadProjects);
+	
 	//app.get('/download', pages.download);
 	//app.get('/upload', pages.upload);
 };
+
+function renderProject() {
+	return (req, res) => {
+		res.render('project', { project: projects.get(req.params.project) });
+	};
+}
