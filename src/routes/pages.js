5748b778973379050563d9a884ffcd23613e6083
Lepidora <miamukherjee2+lepidora@gmail.com>
2018-07-05 17:23:39 +0100

    Cleaned up

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,10 +1,5 @@
 
 /*
- * GET upload page.
+ * GET general page.
  */
 
-/*var download = require('./download.js');
-var upload = require('./upload.js');
-
-exports.download = download.render;
-exports.upload = upload.render;*/
