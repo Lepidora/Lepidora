a6a6d3838c2bca2232020045e1030b7d3fb4cbda
Lepidora <miamukherjee2+lepidora@gmail.com>
2018-08-12 22:39:04 +0100

    Added skills and a footer
    Added a skills section to the index page and added a footer that
    appears on every page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,22 @@
  * GET home page.
  */
 
+const path = require('path');
+
 const projects = require('../controllers/projects.js');
+const skills = require('../projects/skills.json');
+
+skills.forEach((skill) => {
+	if (skill.image) {
+		skill.image = path.join('/images', 'skills', skill.image);
+	}
+});
 
 exports.render = function(req, res) {
 	
 	//Get all projects
-	var projectlist = projects.getAll();
+	var projectlist = projects.getImportant();
 	
 	//Render the index template with all projects
-	res.render('index', { title: 'Lepidora', projects: projectlist });
+	res.render('index', { title: 'Lepidora', projects: projectlist, skills: skills });
 };
